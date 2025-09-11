08334ebd694e4e16827939a3684401f84ae035e8
i5nuc <ip14922@gmail.com>
2025-09-11 21:28:53 +0300

    config, eureka, gateway, front

---
+++ eureka/src/test/java/javamid/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package javamid.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
