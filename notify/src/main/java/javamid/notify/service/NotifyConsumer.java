20831531924d70ed4fb161dbe7da801998b8f595
i5nuc <ip14922@gmail.com>
2025-11-03 18:54:53 +0300

    ended with single leg kafka, start playing with notify to read messages

---
+++ notify/src/main/java/javamid/notify/service/NotifyConsumer.java
@@ -0,0 +1,24 @@
+package javamid.notify.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@Component
+public class NotifyConsumer {
+
+  private static final Logger log = LoggerFactory.getLogger(NotifyConsumer.class);
+
+  @KafkaListener(topics = "notify")
+  public void consume(String message) {
+    log.info("Received notification: {}", message);
+    // Ваша логика обработки уведомления
+    processNotification(message);
+  }
+
+  private void processNotification(String message) {
+    // Преобразуйте JSON в объект, обработайте и т.д.
+    log.info("Processing notification: {}", message);
+  }
+}
