93ca342ccd9d1bb30a3b9f991a69f18642f5d3c9
i5nuc <ip14922@gmail.com>
2025-11-10 22:58:44 +0300

    zipkin

--- exgen/src/main/java/javamid/exgen/config/RestTemplateConfig.java
+++ exgen/src/main/java/javamid/exgen/config/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package javamid.exgen.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
