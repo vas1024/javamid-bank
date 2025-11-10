57348b504af3388e6e9cff67ff85da9c1cbfa6bd
i5nuc <ip14922@gmail.com>
2025-11-10 17:17:39 +0300

    zipkin

--- cash/src/main/java/javamid/cash/config/RestTemplateConfig.java
+++ cash/src/main/java/javamid/cash/config/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package javamid.cash.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -25,7 +26,7 @@ public class RestTemplateConfig {
 
   @Bean
   public RestTemplate restTemplate(RestTemplateBuilder builder) {
-    RestTemplate restTemplate = bilder.build();
+    RestTemplate restTemplate = builder.build();
     restTemplate.setInterceptors(Collections.singletonList(tokenInterceptor));
     return restTemplate;
   }
