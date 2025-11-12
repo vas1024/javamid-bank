e78ea411de7133b9606267e124d82df1abda35b4
i5nuc <ip14922@gmail.com>
2025-11-12 18:33:37 +0300

    prometheus dashboards alarms

--- prometheus/run.sh
+++ prometheus/run.sh
@@ -1,5 +1,12 @@
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+
 helm repo update prometheus-community
-helm install prometheus prometheus-community/kube-prometheus-stack --set admissionWebhooks.enabled=false
+
+helm install prometheus prometheus-community/kube-prometheus-stack \
+  --set admissionWebhooks.enabled=false \
+  --set grafana.sidecar.dashboards.enabled=true \
+  --set grafana.sidecar.dashboards.label=grafana_dashboard
+
 kubectl apply -f servicemonitor.yaml
+
 export GRAFANA_PW=`kubectl --namespace default get secrets prometheus-grafana -o jsonpath="{.data.admin-password}" | base64 -d ; echo`
