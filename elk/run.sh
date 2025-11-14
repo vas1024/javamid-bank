8281e554827afbbb4d2bbe13cfa587cf2c42dddb
i5nuc <ip14922@gmail.com>
2025-11-14 22:42:12 +0300

    kibana

--- elk/run.sh
+++ elk/run.sh
@@ -5,5 +5,8 @@ kind load docker-image elasticsearch:8.11.0
 docker pull kibana:9.1.7     
 kind load docker-image kibana:9.1.7
 git clone https://github.com/elastic/helm-charts.git
-helm install logstash ./helm-charts/logstash -f ./logstash-values.yaml
+
 helm install elasticsearch ./helm-charts/elasticsearch -f elasticsearch-values.yaml
+
+helm install logstash ./helm-charts/logstash -f ./logstash-values.yaml
+helm install kibana ./helm-charts/kibana -f kibana-values.yaml
