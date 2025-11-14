fed9752d8f0e784f5ac16f154941599e139de032
i5nuc <ip14922@gmail.com>
2025-11-14 18:33:49 +0300

    elasticsearch

--- elk/run.sh
+++ elk/run.sh
@@ -1,7 +1,7 @@
 docker pull logstash:9.1.7
 kind load docker-image logstash:9.1.7
-docker pull elasticsearch:9.1.7
-kind load docker-image elasticsearch:9.1.7
+docker pull elasticsearch:8.11.0
+kind load docker-image elasticsearch:8.11.0
 docker pull kibana:9.1.7     
 kind load docker-image kibana:9.1.7
 git clone https://github.com/elastic/helm-charts.git
