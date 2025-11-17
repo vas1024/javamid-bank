1dd21ec1dd645f8e1e5d14036589457d065a2943
i5nuc <ip14922@gmail.com>
2025-11-17 12:56:58 +0300

    tests

--- notify/src/test/java/javamid/notify/TemplateApplicationTests.java
+++ notify/src/test/java/javamid/notify/TemplateApplicationTests.java
@@ -2,15 +2,17 @@ package javamid.notify;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
 
+@SpringBootTest
 @TestPropertySource(properties = {
-				"spring.kafka.listener.auto-startup=false" // Отключаем запуск listener'ов
+				"spring.kafka.listener.auto-startup=false"     // Отключаем запуск listener'ов
 })
 class TemplateApplicationTests {
+
 
 	@Test
 	void contextLoads() {
